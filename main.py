0bc83b114481e09eb8c0566a1093ee9ea4524425
GLinBoy <hojjatabedie@gmail.com>
2022-05-06 22:00:28 +0200

    📦 NEW: imported signature module

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from imp import reload
 import uvicorn
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.responses import RedirectResponse, StreamingResponse
-from io import BytesIOapp = FastAPI()@app.get("/")
+from io import BytesIO
+
+from extractor.signature_extractor import signature_extractor
+
+
+app = FastAPI()
+
+@app.get("/")
 def read_root():
     return RedirectResponse(url='/docs')@app.post("/signatures/extraction")
 async def create_upload_file(file: UploadFile):if __name__ == "__main__":
