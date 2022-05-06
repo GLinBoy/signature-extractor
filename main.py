1862b4169385fb974a84b96631edc150eb654d56
GLinBoy <hojjatabedie@gmail.com>
2022-05-06 21:58:22 +0200

    📦 NEW: Added redirect root to docs (SwaggerUI)

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from imp import reload
 import uvicorn
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.responses import RedirectResponse, StreamingResponse
-from io import BytesIOapp = FastAPI()
+from io import BytesIOapp = FastAPI()@app.get("/")
+def read_root():
+    return RedirectResponse(url='/docs')
