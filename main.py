4e57a1c2d5d3ace6c7e8a8029100b546ff447415
GLinBoy <hojjatabedie@gmail.com>
2022-05-06 21:59:44 +0200

    📦 NEW: Run application with unicorn

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ from fastapi.responses import RedirectResponse, StreamingResponse
 from io import BytesIOapp = FastAPI()@app.get("/")
 def read_root():
     return RedirectResponse(url='/docs')@app.post("/signatures/extraction")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile):if __name__ == "__main__":
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
