2da4adcdddbbfa78ff9a9294dd96a931514e77c1
GLinBoy <hojjatabedie@gmail.com>
2022-05-06 21:57:32 +0200

    📦 NEW: Create FastAPI app

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from imp import reload
 import uvicorn
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.responses import RedirectResponse, StreamingResponse
-from io import BytesIO
+from io import BytesIOapp = FastAPI()
