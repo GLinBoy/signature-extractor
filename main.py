a8457b832c8dd761997942cbe2648c27e1388b37
GLinBoy <hojjatabedie@gmail.com>
2022-05-06 21:59:02 +0200

    📦 NEW: Added method to extract signature from image

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.responses import RedirectResponse, StreamingResponse
 from io import BytesIOapp = FastAPI()@app.get("/")
 def read_root():
-    return RedirectResponse(url='/docs')
+    return RedirectResponse(url='/docs')@app.post("/signatures/extraction")
+async def create_upload_file(file: UploadFile):
